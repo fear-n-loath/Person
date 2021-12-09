2799022ea07adc3c63ed5e5a9426aa48c0ff82a5
yurii <f.societyy98@gmail.com>
2021-12-09 13:03:24 +0200

    create router and page controller, menu, leyout

---
+++ Controller/Adminhtml/Person/Index.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Elogic\Person\Controller\Adminhtml\Person;
+
+use Magento\Backend\App\Action;
+use Magento\Backend\App\Action\Context;
+use Magento\Framework\App\ResponseInterface;
+use Magento\Framework\Controller\ResultInterface;
+use Magento\Framework\View\Result\Page;
+use Magento\Framework\View\Result\PageFactory;
+
+/**
+ * Class Index
+ * @package Elogic\Person\Controller\Adminhtml\Person
+ */
+class Index extends Action
+{
+    /**
+     * @var PageFactory
+     */
+    protected PageFactory $resultPageFactory;
+
+    /**
+     * Index constructor.
+     * @param Context $context
+     * @param PageFactory $resultPageFactory
+     */
+    public function __construct(
+        Context $context,
+        PageFactory $resultPageFactory
+    ) {
+        parent::__construct($context);
+        $this->resultPageFactory = $resultPageFactory;
+    }
+
+    /**
+     * @return ResponseInterface|ResultInterface|Page
+     */
+    public function execute()
+    {
+        $resultPage = $this->resultPageFactory->create();
+        $resultPage->getConfig()->getTitle()->prepend((__('Person')));
+
+        return $resultPage;
+    }
+}
